1988c7e83039e9788be540ae87c319c741c0baf8
Chris Fedun <chris@cfedun.com>
2018-12-11 20:32:47 -0800

    Modify js

--- static/checkout.js
+++ static/checkout.js
@@ -1,3 +1,23 @@
+
+function form_state_field(){
+    if ($("#country").val() === "CA"){
+        $("#state").prev().text("Province");
+        $("#state").next().text("Please provide a valid province.");
+        $("#zip").prev().text("Postal Code");
+        $("#zip").next().text("Postal code required.");
+    } else if ($("#country").val() === "US"){
+        $("#state").prev().text("State");
+        $("#state").next().text("Please provide a valid state.");
+        $("#zip").prev().text("Zip Code");
+        $("#zip").next().text("Zip code required.");
+    }
+}
+
+$( document ).ready( function () {
+    $("#country").on("click", form_state_field);
+    $("#country").on("change", form_state_field);
+});
+
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 $( document ).ready( function() {
     'use strict';
@@ -18,3 +38,4 @@ $( document ).ready( function() {
         });
     }, false);
 });
+
